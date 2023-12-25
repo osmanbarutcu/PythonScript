1fdaafa6e011162ddc5fea133ad63f4b91e5bd45
osmanbarutcu <ozmanbarutcu@gmail.com>
2023-12-25 19:11:06 +0300

    changer mac

--- MacChanger.py
+++ MacChanger.py
@@ -1,13 +1,19 @@
 import subprocess as sp
 import optparse
 
-parse_opject =optparse.OptionParser()
-parse_opject.add_option("i","--interface",dest="interFace",help="interface to change!")
-parse_opject.add_option("m","--mac",dest="macAddress",help="new mac address")
-print("Mac Changer started!")
-interFace="eth0"
-macAddress="00:11:22:33:44:55"
+def GetUserInput():
+    parse_opject =optparse.OptionParser()
+    parse_opject.add_option("i","--interface",dest="interFace",help="interface to change!")
+    parse_opject.add_option("m","--mac",dest="macAddress",help="new mac address")
+    return parse_opject.parse_args()
 
-sp.call(["ifconfig"],interFace,"down")
-sp.call(["ifconfig"], interFace, "hw","ether",macAddress)
-sp.call(["ifconfig"], interFace, "up")
+def ChangeMacAddress(interFace,macAddress):
+    sp.call(["ifconfig"],interFace,"down")
+    sp.call(["ifconfig"],interFace, "hw", "ether",macAddress)
+    sp.call(["ifconfig"],interFace, "up")
+
+
+print("Mac Changer Started!")
+(userInput,arguments)=GetUserInput()
+interFace = userInput.interFace
+macAddress = userInput.macAddress
