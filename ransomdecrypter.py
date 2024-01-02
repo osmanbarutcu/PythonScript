32a8749be5f04305d2179e7646c0c57aaa9d1bc9
osmanbarutcu <ozmanbarutcu@gmail.com>
2024-01-02 17:40:17 +0300

    ransom güncelleme

---
+++ ransomdecrypter.py
@@ -0,0 +1,23 @@
+import os
+from cryptography.fernet import Fernet
+
+files = []
+
+for file in os.listdir():
+	if file == "ransom.py" or file == "generatedkey.key" or file =="ransomdecrypter.py":
+		continue  
+	if os.path.isfile(file):
+		files.append(file)  
+
+print(files)
+
+with open("generatedkey.key","rb") as generatedkey:
+	secret_key = generatedkey.read()
+
+for file in files:
+	with open(file,"rb") as the_file:
+		contents = the_file.read()
+	contents_decrypted = Fernet(secret_key).decrypt(contents)
+	with open(file,"wb") as the_file:
+		the_file.write(contents_decrypted)
+	
